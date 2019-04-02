e00d6f68436c03d2d53ea1d89eabeff4164ebc98
Benjamin <benjaminkleppe@hotmail.com>
2019-04-02 16:13:26 +0200

    Editing credentials works, Tweepy is also connected now.

--- credentials.py
+++ credentials.py
@@ -1,1 +1,21 @@
 # Change credentials
+
+def change_credentials(txt, credentials):
+    with open('credentials.txt','w') as f:
+        f.write("{")
+        f.write("'app_key':'{}',".format(credentials[0]))
+        f.write("'app_secret':'{}',".format(credentials[1]))
+        f.write("'oauth_token':'{}',".format(credentials[2]))
+        f.write("'oauth_token_secret':'{}',".format(credentials[3]))
+        f.write("}")
+
+def main():
+    credentials=[]
+    credentials.append(input("What is your API key?\n"))
+    credentials.append(input("What is your secret API key?\n"))
+    credentials.append(input("What is your OAUTH token?\n"))
+    credentials.append(input("What is your secret OAUTH token?\n"))
+    change_credentials('credentials.txt', credentials)
+
+if __name__ == "__main__":
+    main()
--- twitterapi.py
+++ twitterapi.py
@@ -1,10 +1,25 @@
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def import_credentials(txt):
+    with open(txt,'r') as f:
+        credentials = eval(f.read())
+    return credentials
+    
+def main():
+    credentials = import_credentials('credentials.txt')
+    
+    API_key = credentials['app_key']
+    API_secret = credentials['app_secret']
+    OAUTH_token = credentials['oauth_token']
+    OAUTH_token_secret = credentials['oauth_token_secret']
 
-api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(API_key, API_secret)
+    auth.set_access_token(OAUTH_token, OAUTH_token_secret)
+    api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+    public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        print(tweet.text)
+    
+if __name__ == "__main__":
+    main()
