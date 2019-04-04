e9a7f16a36c426487e0c5c16733cec9d346ca6f0
Benjamin <benjaminkleppe@hotmail.com>
2019-04-04 13:30:11 +0200

    updated version, includes function to load in conversations

--- stream.py
+++ stream.py
@@ -1,6 +1,10 @@
+# Authors: G.Danoe & B.Kleppe
+# Downloads a Twitter stream, customizable search options and analyses the conversations.
+# Date: 03-04-2019
+
 import tweepy
 import json
-import Queue
+import queue
 import threading
 from tweepy import Stream
 from tweepy.streaming import StreamListener
@@ -11,6 +15,38 @@ def import_credentials(txt):
         credentials = eval(f.read())
     return credentials
 
+# Check the replies
+def check_reply(q1_reply_id, list_replies):
+    try:
+        reply_list = list_replies
+        reply = api.get_status(q1_reply_id)
+        text = reply.text
+        reply_list.append(text)
+        new_id = reply.in_reply_to_status_id
+        if new_id != None:
+            check_reply(new_id, reply_list)
+        else:
+            pass
+        return reply_list
+    except tweepy.error.TweepError:
+        return ["This Twitter conversation is private"]
+
+# Go to next conversation
+def next_conversation(queue):
+    q1 = queue.get()
+    q1_reply = q1['in_reply_to_status_id']
+    return check_reply(q1_reply, [q1['text']])
+
+def setQueue():
+    with open('text1.txt', 'r') as output:
+        for line in output:
+            try:
+                newline = json.loads(line)
+                if newline['in_reply_to_status_id']:
+                    q.put(newline)
+            except KeyError:
+                pass
+
 # Setting the credentials
 credentials = import_credentials('credentials.txt')
 API_key = credentials['app_key']
@@ -20,7 +56,10 @@ OAUTH_token_secret = credentials['oauth_token_secret']
 
 # Basic listener, prints the stream to std output
 class Listener(StreamListener):
-
+    def __init__(self, queue):
+        super(Listener, self).__init__()
+        self.queue = queue
+    
     def on_data(self, data):
         with open('text1.txt', 'a') as output:
             output.write(data)
@@ -29,21 +68,39 @@ class Listener(StreamListener):
     def on_error(self, status):
         print(status)
 
+
 if __name__ == '__main__':
-    q = Que.Que()
-    listener = Listener()
+    q = queue.Queue()
+    listener = Listener(q)
     auth = tweepy.OAuthHandler(API_key, API_secret)
     auth.set_access_token(OAUTH_token, OAUTH_token_secret)
+    api = tweepy.API(auth)
     stream = Stream(auth, listener)
     stream.filter(track=["why"],languages=['en'], async=True)
-    with open('text1.txt', 'r') as output:
-        for line in output:
-            newline = json.loads(line)
-            if newline['in_reply_to_status_id']:
-                print(line)
+    setQueue()
+    
+    while True:
+        print(next_conversation(q))
 
 
-    '''with open('text1.json') as output:
-        dat = output.read()
-        for line in dat:
-            print(line)'''
+
+
+
+
+
+'''
+    print(q1['text'])
+    print()
+    print(q2.text)
+    print()
+    print(q3.text)
+
+    
+
+
+    #print(check_reply(q1, 1, []))
+    print(q1['text'])
+    print(api.get_status(stat_id).text)
+    print(n_stat_id)
+    print(api.get_status(n_stat_id).text)'''
+    
