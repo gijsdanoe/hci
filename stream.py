751a927ad9973b0e06c7748eb4a3c73241c593c4
Benjamin <benjaminkleppe@hotmail.com>
2019-04-05 17:04:20 +0200

    now works if files are not in directory aka now makes files. also put everything in a class

--- stream.py
+++ stream.py
@@ -2,11 +2,13 @@
 # Downloads a Twitter stream, customizable search options and analyses the conversations.
 # Date: 03-04-2019
 
+import sys
 import tweepy
 import json
 import queue
 import threading
 import pickle
+import time
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
@@ -16,62 +18,95 @@ def import_credentials(txt):
         credentials = eval(f.read())
     return credentials
 
-# Check the replies
-def check_reply(q1_reply_id, list_replies):
-    try:
-        reply_list = list_replies
-        reply = api.get_status(q1_reply_id)
-        text = reply.text
-        reply_list.append(text)
-        new_id = reply.in_reply_to_status_id
-        if new_id != None:
-            check_reply(new_id, reply_list)
-        else:
-            pass
-        return reply_list[::-1]
-    except tweepy.error.TweepError:
-        return ["This Twitter conversation is private"]
-
-# Go to next conversation
-def next_conversation():
-    q1 = q.get()
-    q1_reply = q1['in_reply_to_status_id']
-    return check_reply(q1_reply, [q1['text']])
-
-def setQueue():
-    with open('text1.txt', 'r') as output:
-        for line in output:
-            try:
-                newline = json.loads(line)
-                if newline['in_reply_to_status_id']:
-                    q.put(newline)
-            except KeyError:
-                pass
-
-def save(conversation, file):
-    if 3 <= len(conversation) <= 10:
-        with open(file, "ab") as f:
-            pickle.dump(conversation, f)
-    else:
-        pass
-
-def get_conversations(file):
-    conversation_list = []
-    with open(file, "rb") as f:
-        while 1:
-            try:
-                conversation = pickle.load(f)
-            except EOFError:
-                break
-            conversation_list.append(conversation)
-    return conversation_list
-
 # Setting the credentials
 credentials = import_credentials('credentials.txt')
 API_key = credentials['app_key']
 API_secret = credentials['app_secret']
 OAUTH_token = credentials['oauth_token']
 OAUTH_token_secret = credentials['oauth_token_secret']
+
+class RawConversations():
+
+    def __init__(self, queue, queue2, listener):
+        self.queue = q
+        self.queue2 = q2
+        self.listener = listener
+
+        self.setQueue()
+
+        self.auth = tweepy.OAuthHandler(API_key, API_secret)
+        self.auth.set_access_token(OAUTH_token, OAUTH_token_secret)
+        self.api = tweepy.API(self.auth)
+        self.stream = Stream(self.auth, listener)
+        self.stream.filter(track=["why"],languages=['en'], async=True)
+            
+        self.thread = threading.Thread(target=self.run, daemon=True).start()
+        self.thread2 = threading.Thread(target=self.conversation_queue, daemon=True).start()
+
+    def run(self):
+        while True:
+            self.save(self.next_conversation())
+
+    # Go to next conversation
+    def next_conversation(self):
+        q1 = self.queue.get()
+        q1_reply = q1['in_reply_to_status_id']
+        return self.check_reply(q1_reply, [q1['text']])
+
+    # Check the replies
+    def check_reply(self, q1_reply_id, list_replies):
+        try:
+            reply_list = list_replies
+            reply = self.api.get_status(q1_reply_id)
+            text = reply.text
+            reply_list.append(text)
+            new_id = reply.in_reply_to_status_id
+            if new_id != None:
+                self.check_reply(new_id, reply_list)
+            else:
+                pass
+            return reply_list[::-1]
+        except tweepy.error.TweepError:
+            return ["This Twitter conversation is private"]
+
+    def setQueue(self):
+        try:
+            with open('text1.txt', 'r') as output:
+                for line in output:
+                    try:
+                        newline = json.loads(line)
+                        if newline['in_reply_to_status_id']:
+                            self.queue.put(newline)
+                    except KeyError:
+                        pass
+        except FileNotFoundError:
+            pass
+
+    def save(self, conversation):
+        try:
+            if 3 <= len(conversation) <= 10:
+                with open("text.pickle", "ab") as f:
+                    pickle.dump(conversation, f)
+            else:
+                pass
+        except FileNotFoundError:
+            pass
+
+
+    def conversation_queue(self):
+        conversation_list = []
+        try:
+            with open("text.pickle", "rb") as f:
+                while 1:
+                    try:
+                        conversation = pickle.load(f)
+                        self.queue2.put(conversation)
+                    except EOFError:
+                        break
+        except FileNotFoundError:
+            time.sleep(4)
+
+
 
 # Basic listener, prints the stream to std output
 class Listener(StreamListener):
@@ -90,24 +125,24 @@ class Listener(StreamListener):
 
 if __name__ == '__main__':
     q = queue.Queue()
+    q2 = queue.Queue()
     listener = Listener(q)
-    auth = tweepy.OAuthHandler(API_key, API_secret)
-    auth.set_access_token(OAUTH_token, OAUTH_token_secret)
-    api = tweepy.API(auth)
-    stream = Stream(auth, listener)
-    stream.filter(track=["why"],languages=['en'], async=True)
-    setQueue()
-
+    x=RawConversations(q, q2, listener)
     
-    while True:
-        threading.Thread(target=save, args=([next_conversation()], "text.pickle"), daemon=True).start()
-        x = get_conversations('text.pickle')
-        print(x)
-    '''with open("text.pickle", "rb") as f:
-        for i in range(10):
-            print(pickle.load(f))
 
-'''
+    while True:
+        inp=input('type something to receive a chat')
+        if inp:
+            if q.empty():
+                print("One second, still loading the comments, please try again in a few seconds")
+                x.setQueue()
+            elif q2.empty():
+                x.save(x.next_conversation())
+                x.conversation_queue()
+            else:
+                print(q2.get())
+
+
 # EXCEPT PICKLE OUTPUT ERR: EOFError:
 
     #with open('output.pickle', 'rb') as output:
